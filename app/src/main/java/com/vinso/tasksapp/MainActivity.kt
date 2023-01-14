2054449bd6df8ecc246303adba195e20de5fb418
Vinso <8vinso8@gmail.com>
2023-01-14 18:45:34 +1000

    Removed default Greeting() function

--- app/src/main/java/com/vinso/tasksapp/MainActivity.kt
+++ app/src/main/java/com/vinso/tasksapp/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.vinso.tasksapp.ui.theme.TasksAppTheme
 
@@ -21,14 +19,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
