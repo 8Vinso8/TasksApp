66939289d21b72ff9430630c6db78732562efb1d
Vinso <8vinso8@gmail.com>
2023-01-14 18:46:02 +1000

    Removed default Surface

--- app/src/main/java/com/vinso/tasksapp/MainActivity.kt
+++ app/src/main/java/com/vinso/tasksapp/MainActivity.kt
@@ -3,10 +3,6 @@ package com.vinso.tasksapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
 import com.vinso.tasksapp.ui.theme.TasksAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,12 +10,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TasksAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                }
             }
         }
     }
