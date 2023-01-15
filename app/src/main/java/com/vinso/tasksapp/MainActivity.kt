4ef5007b234b8a1f8d5b923b11337e54f487b3b0
Vinso <8vinso8@gmail.com>
2023-01-15 17:56:31 +1000

    Add navigation

--- app/src/main/java/com/vinso/tasksapp/MainActivity.kt
+++ app/src/main/java/com/vinso/tasksapp/MainActivity.kt
@@ -3,13 +3,49 @@ package com.vinso.tasksapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.vinso.tasksapp.ui.add_edit_task.AddEditTaskScreen
+import com.vinso.tasksapp.ui.task_list.TaskListScreen
 import com.vinso.tasksapp.ui.theme.TasksAppTheme
+import com.vinso.tasksapp.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TasksAppTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TASK_LIST
+                ) {
+                    composable(Routes.TASK_LIST) {
+                        TaskListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TASK + "?taskId={taskId}",
+                        arguments = listOf(
+                            navArgument(name = "taskId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTaskScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
+                }
             }
         }
     }
