86c7812cd28083cad17d34a71af28647a152c165
Vinso <8vinso8@gmail.com>
2023-01-15 17:52:04 +1000

    Add task data access object

--- app/src/main/java/com/vinso/tasksapp/data/TaskDao.kt
+++ app/src/main/java/com/vinso/tasksapp/data/TaskDao.kt
@@ -5,15 +5,16 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(task: Task)
 
     @Delete
     suspend fun deleteTask(task: Task)
 
-    @Query("Select * FROM task WHERE id = :id")
+    @Query("SELECT * FROM task WHERE id = :id")
     suspend fun getTaskById(id: Int): Task?
 
-    @Query("Select * FROM task")
+    @Query("SELECT * FROM task")
     fun getTasks(): Flow<List<Task>>
 }
