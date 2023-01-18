0ab54fe8b10ed9e34b2560d8c57ca0cade134e14
Vinso <8vinso8@gmail.com>
2023-01-18 20:18:30 +1000

    Add list id to task model

--- app/src/main/java/com/vinso/tasksapp/data/Task.kt
+++ app/src/main/java/com/vinso/tasksapp/data/Task.kt
@@ -2,9 +2,10 @@ package com.vinso.tasksapp.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "tasks")
 data class Task(
     @ColumnInfo(name = "name")
     val name: String,
@@ -18,6 +19,8 @@ data class Task(
     val isSubtask: Boolean = false,
     @ColumnInfo(name = "mainId")
     val MainTaskId: Int? = null,
+    @ColumnInfo(name = "listId")
+    val listId: Int,
     @ColumnInfo(name = "id")
     @PrimaryKey val id: Int? = null
 )
