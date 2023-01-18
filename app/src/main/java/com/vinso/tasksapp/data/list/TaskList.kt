ce676a020b5f1d8289204ed19c111f673b42c9ac
Vinso <8vinso8@gmail.com>
2023-01-19 00:02:49 +1000

    Renamed to not conflict with list container

--- app/src/main/java/com/vinso/tasksapp/data/list/TaskList.kt
+++ app/src/main/java/com/vinso/tasksapp/data/list/TaskList.kt
@@ -1,11 +1,11 @@
-package com.vinso.tasksapp.data
+package com.vinso.tasksapp.data.list
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "lists")
-data class List(
+data class TaskList(
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "id") @PrimaryKey val id: Int? = null
 )
