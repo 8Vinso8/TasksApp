339a47c892e4d80ebd73649268a8592c19c8424f
Vinso <8vinso8@gmail.com>
2023-01-18 20:07:16 +1000

    Add custom column names to all fields

--- app/src/main/java/com/vinso/tasksapp/data/Task.kt
+++ app/src/main/java/com/vinso/tasksapp/data/Task.kt
@@ -1,15 +1,23 @@
 package com.vinso.tasksapp.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class Task(
+    @ColumnInfo(name = "name")
     val name: String,
+    @ColumnInfo(name = "description")
     val description: String?,
+    @ColumnInfo(name = "isDone")
     val isDone: Boolean,
+    @ColumnInfo(name = "isFavourite")
     val isFavourite: Boolean,
+    @ColumnInfo(name = "isSub")
     val isSubtask: Boolean = false,
+    @ColumnInfo(name = "mainId")
     val MainTaskId: Int? = null,
+    @ColumnInfo(name = "id")
     @PrimaryKey val id: Int? = null
 )
